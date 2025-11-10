eeff76d56eab3a28b36c3e76d354f1bd3f13abcb
roger <maitebst@alumnes.ilerna.com>
2025-11-10 19:36:04 +0100

    codi final

--- HolaGit.java
+++ HolaGit.java
@@ -1,5 +1,8 @@
 public class HolaGit {
     public static void main(String[] args) {
         System.out.println("Hola, text resol roger, alvaro i marc);
+	System.out.println("Hola, sóc el Roger i estic fent el primer GitH!");
+	System.out.println("Hola SOY SHAI y Alvaro");
+	System.out.println("i love derrick rose (marc)");
     }
 }
