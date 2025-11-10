6a72ec8572eadbced7f8a5acde10201bd82f64ef
roger <maitebst@alumnes.ilerna.com>
2025-11-10 17:31:27 +0100

    Text personal Roger

--- HolaGit.java
+++ HolaGit.java
@@ -1,5 +1,5 @@
 public class HolaGit {
     public static void main(String[] args) {
-        System.out.println("Hola GitH!");
+        System.out.println("Hola, sóc el Roger i estic fent el primer GitH!");
     }
 }
